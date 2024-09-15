a9f59b1579a561ec892e265c6e34e78a67dd1cba
Aleksandra Popek <olek@rafkos.net>
2024-09-15 22:18:53 +0200

    Posts comments added

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_page, name="login"),
+    path('register/', views.register_page, name="register"),
     path('logout/', views.logout_user, name="logout"),
     path('', views.home, name="home"), 
     path('thread/<str:thread_pk>/', views.thread, name="thread"), 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('update-thread/<str:thread_pk>', views.update_thread, name="update-thread"), 
     path('create-post/', views.create_post, name="create-post"), 
     path('update-post/<str:post_pk>', views.update_post, name="update-post"), 
-    path('delete-post/<str:post_pk>', views.delete_post, name="delete-post")
+    path('delete-post/<str:post_pk>', views.delete_post, name="delete-post"), 
+    path('load-more-comments/<str:post_id>/', views.load_more_comments, name='load_more_comments')
 ]
--- base/views.py
+++ base/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Tag
 from .forms import ThreadForm
 
 # Create your views here.
 def login_page(request):
 
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -28,7 +35,7 @@ def login_page(request):
         else:
             messages.error(request, 'Username or password does not exist.')
 
-    context={}
+    context={'page': page}
     return render(request, 'base/login_register.html', context)
 
 
@@ -37,12 +44,30 @@ def logout_user(request):
     return redirect("home")
 
 
+def register_page(request):
+    page = 'register'
+    form = UserCreationForm()
+    context = {'page': page, 'form': form}
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else: 
+            messages.error(request, "An error occured during registration.")
+
+    return render(request, 'base/login_register.html', context=context)
+
+
 def home(request):
 
     q = request.GET.get("q") if "q" in request.GET else ""
     tag_name = request.GET.get("tag", "").upper() if "tag" in request.GET else ""
 
-    print("request", request.GET)
     if tag_name != "":
         tag_value = getattr(Tag.TagType, tag_name, None)
         threads = Post.objects.filter(post_type="t").filter(tag__name=tag_value).prefetch_related('tag')
@@ -77,7 +102,8 @@ def thread(request, thread_pk):
 def post(request, thread_pk, post_pk):
     post = Post.objects.get(post_id=post_pk)
     thread = Post.objects.get(post_id=thread_pk)
-    context = {'post': post, 'thread': thread}
+    comments = Post.objects.filter(parent_post=post_pk)[:5]
+    context = {'post': post, 'thread': thread, 'comments': comments}
 
     return render(request, 'base/post.html', context=context)
 
@@ -174,3 +200,29 @@ def delete_post(request, post_pk):
             return redirect('home')
     return render(request, "base/delete.html", {'obj': post})
 
+
+def load_more_comments(request, post_id):
+    parent_post = get_object_or_404(Post, post_id=post_id)
+    # post_id = str(request.GET.get('post_id', 0))
+    offset = int(request.GET.get('offset', 0))
+    limit = 5
+
+    all_comments = Post.objects.filter(parent_post=post_id)
+    comments = all_comments[offset:offset+limit]
+
+    comments_content = []
+
+    for com in comments:
+        reply_count = Post.objects.filter(parent_post=com.post_id).count()
+        comments_content.append({
+            'author': str(com.author), 
+            'title': com.title,
+            'content': com.content, 
+            'comment_id': com.post_id, 
+            'reply_count': reply_count
+        })
+        
+    more_comments = all_comments.count() > offset + limit
+
+    context = {'comments': comments_content, "more_comments": more_comments}
+    return JsonResponse(context)
